233d1f5a7c447d11335ddb903bb3112eb7eac0b9
Matt Wistrand <matt.wistrand@gmail.com>
2021-11-01 09:54:31 -0500

    Update lualine config

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,18 @@
 require('lualine').setup({
   options = {
-    theme = 'dracula'
-  }
+    theme = 'dracula',
+	component_separators = { left = '•', right = '•' }
+  },
+  sections = {
+	lualine_a = {'mode'},
+	lualine_b = {'FugitiveHead', {'diagnostics', sources={'nvim_lsp', 'coc'}}},
+	lualine_c = {'filename'},
+	lualine_x = {
+	  'encoding',
+	  {'filetype', icons_enabled = false}
+	},
+	lualine_y = {'progress'},
+	lualine_z = {'location'}
+  },
+  extensions = {'fugitive', 'nvim-tree'}
 })
