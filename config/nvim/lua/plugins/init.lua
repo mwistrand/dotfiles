2072911ef74d24b02524abfd5fb440448b029851
Matt Wistrand <matt.wistrand@gmail.com>
2021-11-02 17:12:38 -0500

    NeoVim plugin cleanup
    - Comment out vim-stamp as I never actually use it
    - Add yajs and vim-node for JavaScript; remove vim-javascript
    - Remove vim-css-color plugin
    - Replace vim-signify with gitsigns

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -20,8 +20,9 @@ cmd [[Plug 'RRethy/nvim-base16']]
 cmd [[Plug 'tpope/vim-commentary']]
 
 -- VCS management
-cmd [[Plug 'mhinz/vim-signify']]
 cmd [[Plug 'tpope/vim-fugitive']]
+cmd [[Plug 'nvim-lua/plenary.nvim']]
+cmd [[Plug 'lewis6991/gitsigns.nvim']]
 
 -- Toggle comments with gcc
 cmd [[Plug 'tpope/vim-commentary']]
@@ -30,7 +31,7 @@ cmd [[Plug 'tpope/vim-commentary']]
 cmd [[Plug 'tpope/vim-sleuth']]
 
 -- Additional motions for working with the delete register
-cmd [[Plug 'LandonSchropp/vim-stamp']]
+-- cmd [[Plug 'LandonSchropp/vim-stamp']]
 
 -- Repeat last motion with `.`
 cmd [[Plug 'tpope/vim-repeat']]
@@ -54,9 +55,9 @@ cmd [[Plug 'nvim-lualine/lualine.nvim']]
 
 -- Language-specific plugins
 -- CSS
-cmd [[Plug 'ap/vim-css-color', { 'for': 'css' }]]
 cmd [[Plug 'cakebaker/scss-syntax.vim', { 'for': 'scss' }]]
 cmd [[Plug 'hail2u/vim-css3-syntax', { 'for': 'css' }]]
+cmd [[Plug 'stephenway/postcss.vim', { 'for': 'css' }]]
 cmd [[Plug 'groenewege/vim-less', { 'for': 'less' }]]
 cmd [[Plug 'wavded/vim-stylus', { 'for': ['stylus', 'markdown'] }]]
 
@@ -68,8 +69,9 @@ cmd [[Plug 'othree/html5.vim', { 'for': 'html' }]]
 
 -- JavaScript
 cmd [[Plug 'elzr/vim-json', { 'for': 'json' }]]
-cmd [[Plug 'pangloss/vim-javascript', { 'for': ['javascript', 'html'] }]]
 cmd [[Plug 'MaxMEllon/vim-jsx-pretty']]
+cmd [[Plug 'othree/yajs.vim', { 'for': [ 'javascript', 'javascript.jsx', 'html' ] }]]
+cmd [[Plug 'moll/vim-node', { 'for': 'javascript' }]]
 
 -- TypeScript
 cmd [[Plug 'leafgarland/typescript-vim', { 'for': ['typescript', 'typescript.tsx'] }]]
@@ -99,5 +101,6 @@ end
 require('plugins.coc')
 require('plugins.formatter')
 require('plugins.fzf')
+require('plugins.gitsigns')
 require('plugins.lualine')
 require('plugins.nvimtree')
