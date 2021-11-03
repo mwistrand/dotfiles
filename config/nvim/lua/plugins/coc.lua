c5948a5d59adcf829af66cc118cfabceae4caabc
Matt Wistrand <matt.wistrand@gmail.com>
2021-11-03 08:09:19 -0500

    Simplify autocomplete tab navigation

--- config/nvim/lua/plugins/coc.lua
+++ config/nvim/lua/plugins/coc.lua
@@ -28,32 +28,8 @@ utils.nmap('gf', '<Plug>(coc-definition)')
 utils.nmap('gi', '<Plug>(coc-implementation)')
 utils.nmap('gr', '<Plug>(coc-references)')
 
-local function check_back_space()
-  local col = vim.api.nvim_win_get_cursor(0)[2]
-  return (col == 0 or vim.api.nvim_get_current_line():sub(col, col):match('%s')) and true
-end
-
--- h/t https://github.com/smauel/dotfiles/blob/master/nvim/lua/config/coc.lua
-function CocSmartTab()
-  if fn.pumvisible() == 1 then
-    return termcodes('<C-n>')
-  elseif fn['coc#expandableOrJumpable']() == 1 then
-    return termcodes('<C-r>') .. [[=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])]] .. termcodes('<CR>')
-  else
-    local status, result = pcall(check_back_space)
-    if status and result then
-      return termcodes('<Tab>')
-    else
-      return fn['coc#refresh()']()
-    end
-  end
-end
-
--- When popup menu is visible, tab goes to next entry.
--- Else, if the cursor is in an active snippet, tab between fields.
--- Else, if the character before the cursor isn't whitespace, put a Tab.
--- Else, refresh the completion list
-utils.inoremap('<Tab>', 'v:lua.CocSmartTab()', { expr = true })
+-- When popup menu is visible, tab goes to next entry; otherwise, ignore
+utils.inoremap('<Tab>', "pumvisible() ? '<c-n>' : '\\<tab>'", { expr = true })
 
 -- Shift-Tab for cycling backwards through matches in a completion popup
 utils.inoremap('<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<C-h>"', { expr = true })
