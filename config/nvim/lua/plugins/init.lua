116ad4de1077dc72e4f10cc5b0786cf2db7697fb
Matt Wistrand <matt.wistrand@gmail.com>
2025-02-22 08:47:52 -0600

    feat: iron.nvim for python

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -172,6 +172,46 @@ require('lazy').setup({
 		end
 	},
 
+	-- Python
+	{
+		'Vigemus/iron.nvim',
+		ft = 'python',
+		config = function()
+			require('iron.core').setup({
+				config = {
+					scratch_repl = true,
+					repl_definition = {
+						python = {
+							command = { 'python3' },
+							format = require('iron.fts.common').bracketed_paste_python,
+						},
+					},
+					repl_open_cmd = require('iron.view').right(80),
+					repl_filetype = function(bufnr, ft)
+						return ft
+					end,
+				},
+				highlight = {
+					italic = true,
+				},
+				keymaps = {
+					restart_repl = '<LocalLeader>c',
+					toggle_repl = '<LocalLeader>dd',
+					clear = '<LocalLeader>dc',
+					interrupt = '<LocalLeader>ds',
+					exit = '<LocalLeader>dq',
+					visual_send = '<LocalLeader>ee',
+					send_line = '<LocalLeader>el',
+					send_code_block = '<LocalLeader>ee',
+					send_file = '<LocalLeader>ef',
+					send_until_cursor = '<LocalLeader>ec',
+					cr = '<LocalLeader>e<cr>',
+				}
+			})
+			utils.nmap('<LocalLeader>df', '<cmd>IronFocus<cr>')
+		end
+	},
+
 	-- Java
 	-- { 'vim-test/vim-test', ft = 'java' },
 	'mfussenegger/nvim-jdtls',
