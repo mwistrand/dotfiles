c13d7700ae9b0ce568e7f363bb7ba9ec99172925
Matt Wistrand <matt.wistrand@gmail.com>
2025-03-23 06:09:41 -0500

    feat: add boole

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -225,6 +225,16 @@ require('lazy').setup({
 		}
 	},
 
+	{
+		'nat-418/boole.nvim',
+		opts = {
+			mappings = {
+				increment = '<C-a>',
+				decrement = '<C-x>',
+			},
+		},
+	},
+
 	-- Improved Unicode Support
 	{
 		'chrisbra/unicode.vim',
